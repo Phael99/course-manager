47d028873ea7b26ed51d5a38a073dc949f6e859e
Phael99 <raphael.alexsander99@gmail.com>
2021-08-23 18:14:00 -0300

    comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,47 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './404/error-404.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseListComponent } from './courses/course-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ReplacePipe } from './pipe/replace.pipe';
+import { StarComponent } from './shared/components/star/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    Error404Component,
+    CourseInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      
+      {
+        path: '**', component: Error404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
